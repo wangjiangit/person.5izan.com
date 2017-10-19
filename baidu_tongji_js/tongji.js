32996760963be9b5e21fc506b1b01123035bfc65
wang jian <jiji_12342006@163.com>
2017-10-19 15:51:17 +0800

    添加百度统计代码

---
+++ baidu_tongji_js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?b549465c85adfcc8cc6d47cc6c6814c4";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
